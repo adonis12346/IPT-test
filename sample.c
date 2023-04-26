7148a3b18677812e5b270861022463c5187d560c
Adonis Virtudazo <adonis.virtudazo@urios.edu.ph>
2023-04-26 06:09:18 +0000

    add new code

--- sample.c
+++ sample.c
@@ -1,7 +1,9 @@
 #include  <studio.h> 
 
 int main() { 
-	printf("sumilac");
-	printf("Im Bas");
-	printf("Karl Bejerano");
+printf("sumilac");
+printf("Im Bas");
+printf("Karl Bejerano");
+printf("from branch staging"); 
+printf(" add new features"); 
 }
